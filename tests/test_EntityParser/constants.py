eae0b7910d7d600110237a4f1a7be8606de552ae
elebur <elebur249@proton.me>
2025-04-07 19:23:04 +0300

    test: add Markdown V2 error messages

--- tests/test_EntityParser/constants.py
+++ tests/test_EntityParser/constants.py
@@ -1,7 +1,12 @@
 ERR_MSG_CANT_PARSE_ENTITY_MD_V1 = ("Can't parse entities: can't find end of "
                                    "the entity starting at byte offset {offset}")
 
+ERR_MSG_CANT_PARSE_ENTITY_MD_V2 = ("Can't parse entities: can't find end of {entity_type}"
+                                   " entity at byte offset {offset}")
+
 ERR_MSG_EMPTY_STR_MD_V1 = "Text must be non\-empty"
+
+ERR_MSG_EMPTY_STR_MD_V2 = "Message text is empty"
 
 ERR_MSG_CHAR_MUST_BE_ESCAPED = ("Can't parse entities: character '\{0}' is reserved"
                                 " and must be escaped with the preceding '\\'")
